e397f9f395eec3a275d75e418af12227029231a3
Jchesley21 <79483437+Jchesley21@users.noreply.github.com>
2023-01-24 10:21:43 -0700

    additional commist for js models

---
+++ models/role.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class role extends Model {}
+
+role.init(
+  {
+    role_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING
+    },
+    salary: {
+      type: DataTypes.DECIMAL
+    },
+    department_id: {
+      type: DataTypes.INTEGER
+    }    
+    
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'role'
+  }
+);
+
+module.exports = role;
