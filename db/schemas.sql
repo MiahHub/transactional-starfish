596d29d53291c86e1060a18c02c377a6844fd03d
Jchesley21 <79483437+Jchesley21@users.noreply.github.com>
2023-01-24 14:39:12 -0700

    more schemas and seed data

--- db/schemas.sql
+++ db/schemas.sql
@@ -1,2 +1,22 @@
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+  id INT NOT NULL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+CREATE TABLE role (
+  id INT NOT NULL PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT
+);
+CREATE TABLE employee (
+  id INT NOT NULL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT
+);
